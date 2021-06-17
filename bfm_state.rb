e4427ae55ded3fee96ade96e528a46a0ab461a7a
primenumber <prime@kmc.gr.jp>
2021-06-17 15:37:16 +0900

    Refactor

--- bfm_state.rb
+++ bfm_state.rb
@@ -1,8 +1,8 @@
 class BFMState
   attr_reader :ary, :data_pos, :prog_pos
-  @@code = "><+-[],.\0"
+  INSTS = "><+-[],.\0".bytes
   def initialize(code, input=$stdin.each_byte, output=$stdout)
-    @ary = Hash.new("\0")
+    @ary = Hash.new(0)
     code.each_byte.with_index {|ch,i| @ary[i] = ch}
     @data_pos = 0
     @prog_pos = 0
@@ -14,10 +14,10 @@ class BFMState
     while cnt != 0
       @prog_pos -= 1
       raise "Unmatch brackets" if !@ary.has_key?(@prog_pos)
-      case @ary[@prog_pos].chr
-      when @@code[4] then
+      case @ary[@prog_pos]
+      when INSTS[4] then
         cnt += 1
-      when @@code[5] then
+      when INSTS[5] then
         cnt -= 1
       end
     end
@@ -27,51 +27,50 @@ class BFMState
     while cnt != 0
       @prog_pos += 1
       raise "Unmatch brackets" if !@ary.has_key?(@prog_pos)
-      case @ary[@prog_pos].chr
-      when @@code[4] then
+      case @ary[@prog_pos]
+      when INSTS[4] then
         cnt += 1
-      when @@code[5] then
+      when INSTS[5] then
         cnt -= 1
       end
     end
   end
   def step
-    case @ary[@prog_pos].chr
-    when @@code[0] then
+    case @ary[@prog_pos]
+    when INSTS[0] then
       @data_pos += 1
       @ary[@data_pos] = 0 if !@ary.has_key?(@data_pos)
-    when @@code[1] then
+    when INSTS[1] then
       @data_pos -= 1
       @ary[@data_pos] = 0 if !@ary.has_key?(@data_pos)
-    when @@code[2] then
+    when INSTS[2] then
       if @ary[@data_pos] < 255 then
         @ary[@data_pos] += 1
       else
         @ary[@data_pos] = 0
       end
-    when @@code[3] then
+    when INSTS[3] then
       if @ary[@data_pos] > 0 then
         @ary[@data_pos] -= 1
       else
         @ary[@data_pos] = 255
       end
-    when @@code[4] then
+    when INSTS[4] then
       move_to_close if @ary[@data_pos] == 0
-    when @@code[5] then
+    when INSTS[5] then
       move_to_open if @ary[@data_pos] != 0
-    when @@code[6] then
+    when INSTS[6] then
       begin
         @ary[@data_pos] = @input_enum.next
       rescue StopIteration
         @ary[@data_pos] = 0
       end
-    when @@code[7] then
+    when INSTS[7] then
       @output.putc @ary[@data_pos].chr
-    when @@code[8] then
+    when INSTS[8] then
       return false
     end
     @prog_pos += 1
-    @ary[@prog_pos] = 0 if !@ary.has_key?(@prog_pos)
     return true
   end
 end
